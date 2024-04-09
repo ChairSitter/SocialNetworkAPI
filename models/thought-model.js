55f9476c9d921eca2bfe33c1663a997023ce3a53
Bryan Schneller <bryan.schneller@gmail.com>
2024-04-09 14:41:08 -0400

    Remove some comments

--- models/thought-model.js
+++ models/thought-model.js
@@ -33,7 +33,7 @@ const thoughtSchema = new mongoose.Schema({
 
 thoughtSchema.virtual('reactionCount').get(function () {
     return this.reactions.length;
-}); //TODO ensure this works
+});
 
 const Thought = mongoose.model('Thought', thoughtSchema);
 
